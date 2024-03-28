354c77a9acbc7ac08efc7b0ea45fa54f94fb1a52
Connor Hanan <connor.hanan@schraadinc.com>
2024-03-28 13:21:37 -0500

    Update - Power BI
    Model has been loaded into PBI & now I need to continue to build visuals

--- PBI_script.py
+++ PBI_script.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import pickle
 
+# Loading random forest model
 file = open(r"C:\Users\connor.hanan\workspace\Python\Projects\diamond_price_prediction\Resources\random_forest_model.pkl", 'rb')
 model = pickle.load(file)
 
-df = dataset
-d_df = pd.get_dummies(df)
-X = d_df.drop(['price', 'x', 'y', 'z'], axis=1)
-df['predictions'] = model.predict(X)
+# Feature Engineering
+d_dataset = pd.get_dummies(dataset)
+d_dataset = d_dataset.dropna(axis=1)
+X = d_dataset.drop(['price', 'x', 'y', 'z'], axis=1)
+dataset['predictions'] = model.predict(X)
