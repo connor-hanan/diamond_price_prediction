aba7e7a9721dc70397cd9644a2a9956de9aa134b
connor-hanan <connorhanan@gmail.com>
2024-05-31 13:59:49 -0500

    dev: importing model into power bi

--- resources/pbi_script.py
+++ resources/pbi_script.py
@@ -1,12 +1,19 @@
+# 'dataset' holds the input data for this script
 import pandas as pd
 import pickle
 
-# Loading random forest model
-file = open(r"C:\Users\connor.hanan\workspace\Python\Projects\diamond_price_prediction\Resources\random_forest_model.pkl", 'rb')
-model = pickle.load(file)
+# Loading random forest model & scaler
+file_path = r"C:\Users\conno\workspace\projects\diamond_price_prediction\resources\random_forest_model.pkl"
+scaler_path = r"C:\Users\conno\workspace\projects\diamond_price_prediction\resources\scaler.pkl"
+with open(file_path, 'rb') as file:
+    model = pickle.load(file)
+with open(scaler_path, 'rb') as scaler_file:
+    scaler = pickle.load(scaler_file)
 
 # Feature Engineering
 d_dataset = pd.get_dummies(dataset)
-d_dataset = d_dataset.dropna(axis=1)
-X = d_dataset.drop(['price', 'x', 'y', 'z'], axis=1)
+d_dataset = d_dataset.drop(['price', 'x', 'y', 'z'], axis=1)
+X = scaler.transform(d_dataset)
+
+# Make predictions
 dataset['predictions'] = model.predict(X)
